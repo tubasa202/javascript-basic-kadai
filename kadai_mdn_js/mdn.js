9cbfcbded864ffa5b18a190db06fb5d0ff0d0fd4
tubasa202 <tubasa13171225@gmail.com>
2023-10-26 09:51:31 +0900

    課題提出

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,7 @@
+const day = new Date('September 22, 2023 23:15:30');
+
+const date1 =day.getFullYear();
+const date2 =day.getMonth();
+const date3 = day.getDate();
+
+console.log(date1 + '年' + date2 +'月'+ date3 + '日');
